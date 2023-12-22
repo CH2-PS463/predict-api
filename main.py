8f759dafc5b5fb650722135e413d8ef5c0a4d367
IndraRusyana <indrarusyana@gmail.com>
2023-12-22 16:38:11 +0700

    bug fixxed

--- main.py
+++ main.py
@@ -3,27 +3,25 @@ from google.cloud import storage
 import tensorflow as tf
 from io import BytesIO
 from flask import Flask, request, jsonify
-import streamlit as st
+from keras.models import load_model
 import numpy as np
+from tensorflow.keras.applications.mobilenet import preprocess_input
 
-#connection to gcp
 app = Flask(__name__)
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'capstone-satriasayur.json'
 storage_client = storage.Client()
 
-#Tensorflow Model Prediction
-def model_prediction(test_image):
-    model = tf.keras.models.load_model("trained_model.h5")
-    image = tf.keras.preprocessing.image.load_img(test_image,target_size=(64,64))
-    input_arr = tf.keras.preprocessing.image.img_to_array(image)
-    input_arr = np.array([input_arr]) #convert single image to batch
-    predictions = model.predict(input_arr)
-    return np.argmax(predictions) #return index of max element
+def req(y_true, y_pred):
+    req = tf.metrics.req(y_true, y_pred)[1]
+    tf.keras.backend.get_session().run(tf.local_variables_initializer())
+    return req
+
+model = load_model('lite_model.tflite', custom_objects={'req': req})
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        #cek if bucket is ready
         try:
             image_bucket = storage_client.get_bucket(
                 'satriasayur-bucket')
@@ -34,19 +32,32 @@ def index():
             respond = jsonify({'message': 'Error loading image file'})
             respond.status_code = 400
             return respond
-        
-        #Reading Labels
-        result_index = model_prediction(img_path)
-        with open("labels.txt") as f:
-            content = f.readlines()
-        label = []
-        for i in content:
-            label.append(i[:-1])
-        st.success("Model is Predicting it's a {}".format(label[result_index]))
 
-        #output model
+        image = tf.keras.preprocessing.image.load_img(img_path ,target_size=(64,64))
+        input_arr = tf.keras.preprocessing.image.img_to_array(image)
+        input_arr = np.array([input_arr]) #convert single image to batch
+
+        # model predict
+        pred_vegetables = model.predict(input_arr)
+        # find the max prediction of the image
+        maxx = pred_vegetables.max()
+
+        nama = ['Bean', 'Bitter_Gourd', 'Bottle_Gourd',
+                'Brinjal', 'Broccoli', 'Cabbage','Capsicum',
+                'Carrot','Cauliflower','Cucumber','Papaya',
+                'Potato','Pumpkin','Radish','Tomato']
+
+        # for respond output from prediction if predict <=0.4
+        if maxx <= 0.75:
+            respond = jsonify({
+                'message': 'Sayuran tidak terdeteksi'
+            })
+            respond.status_code = 400
+            return respond
+
         result = {
-            "nama": format(label[result_index]),
+            "name": nama[np.argmax(pred_vegetables)],
+            "value_var": print(pred_vegetables)
         }
 
         respond = jsonify(result)
